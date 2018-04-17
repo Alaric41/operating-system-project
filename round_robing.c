6e5ef2e9933340ba7644d17d7a3997a9bbcdbfb4
Alaric41 <38439115+Alaric41@users.noreply.github.com>
2018-04-17 19:09:00 +0530

    Update round_robing.c

--- round_robing.c
+++ round_robing.c
@@ -1,70 +1,135 @@
-#include<stdio.h>
- 
-int main()
-{
-    int pr[20],bt[20],p[20],wt[20],tat[20],i,j,n,total=0,pos,temp;
-    float avg_wt,avg_tat;
-    printf("Enter number of process:");
-    scanf("%d",&n);
- 
-    printf("\nEnter Burst Time and Priority:\n");
-    for(i=0;i<n;i++)
-    {
-        printf("\nProcess   %d ",i+1);
-        printf("\nBurst Time     :  ");
-        scanf("%d",&bt[i]);
-        printf("Priority       :  ");
-        scanf("%d",&pr[i]);
-        p[i]=i+1;           //contains process number
-    }
- 
-    //sorting burst time in ascending order using selection sort
-    for(i=0;i<n;i++)
-    {
-        pos=i;
-        for(j=i+1;j<n;j++)
-        {
-            if(pr[j]>pr[pos])
-                pos=j;
-        }
- 
-        temp=pr[i];
-        pr[i]=pr[pos];
-        pr[pos]=temp;
+/* Round Robin Scheduling Simulation*/
 
-        temp=bt[i];
-        bt[i]=bt[pos];
-        bt[pos]=temp;
- 
-        temp=p[i];
-        p[i]=p[pos];
-        p[pos]=temp;
-    }
- 
-    wt[0]=0;            //waiting time for first process will be zero
- 
-    //calculate waiting time
-    for(i=1;i<n;i++)
-    {
-        wt[i]=0;
-        for(j=0;j<i;j++)
-            wt[i]+=bt[j];
- 
-        total+=wt[i];
-    }
- 
-    avg_wt=(float)total/n;      //average waiting time
-    total=0;
- 
-    printf("\nProcess\t    Burst Time    \tWaiting Time\tTurnaround Time     Priority");
-    for(i=0;i<n;i++)
-    {
-        tat[i]=bt[i]+wt[i];     //calculate turnaround time
-        total+=tat[i];
-        printf("\np%d\t\t  %d\t\t    %d\t\t\t%d   \t        %d",p[i],bt[i],wt[i],tat[i],pr[i]);
-    }
- 
-    avg_tat=(float)total/n;     //average turnaround time
-    printf("\n\nAverage Waiting Time=%f",avg_wt);
-    printf("\nAverage Turnaround Time=%f\n",avg_tat);
+#include <stdio.h>
+#include <stdlib.h>
+/* Process Data Structure */
+struct process {
+int pid; /* Process ID */
+int burst; /* CPU Burst Time */
+int priority; /* Priority */
+int working; /* Working time, for round-robin scheduling */
+int waiting; /* Waiting time, for round-robin scheduling */
+struct process *next;
+};
+/* Function Prototype Declarations */
+struct process *init_process (int pid, int burst, int priority);
+void listprocs (struct process *proc);
+void rr (struct process *proc, int quantum);
+/* Main Program Segment */
+int main (void) {
+/* Initialize process list */
+struct process *plist, *ptmp;
+plist = init_process(1, 10, 3);
+plist->next = init_process(2, 1, 1); ptmp = plist->next;
+ptmp->next = init_process(3, 2, 3); ptmp = ptmp->next;
+ptmp->next = init_process(4, 1, 4); ptmp = ptmp->next;
+ptmp->next = init_process(5, 5, 2);
+/* Perform simulations */
+listprocs(plist);
+rr(plist, 1);
+/* Terminate cleanly */
+while (plist != NULL) {
+ptmp = plist;
+plist = plist->next;
+free(ptmp);
+};
+return(0);
+};
+/* Process list entry initialization routine */
+
+struct process *init_process (int pid, int burst, int priority) {
+struct process *proc;
+proc = malloc(sizeof(struct process));
+if (proc == NULL) {
+printf("Fatal error: memory allocation failure.\nTerminating.\n");
+exit(1);
+};
+proc->pid = pid;
+proc->burst = burst;
+proc->priority = priority;
+proc->working = 0;
+proc->waiting = 0;
+proc->next = NULL;
+return(proc);
+};
+/* Process listing */
+void listprocs (struct process *proc) {
+struct process *tmp = proc;
+
+printf("BEGIN:\tProcess Listing\n");
+
+while (tmp != NULL) {
+printf("PID: %d\t\tPriority: %d\tBurst: %d\n", tmp->pid, tmp->priority, tmp->burst);
+tmp = tmp->next;
+};
+
+printf("END:\tProcess Listing\n\n");
+}; 
+/* Round-Robin scheduling simulation */
+void rr (struct process *proc, int quantum) {
+int jobsremain, passes;
+struct process *copy, *tmpsrc, *tmp, *slot;
+
+printf("BEGIN:\tRound-Robin scheduling simulation (Quantum: %d)\n", quantum);
+/* Duplicate process list */
+tmpsrc = proc;
+copy = tmp = NULL;
+while (tmpsrc != NULL) {
+if (copy == NULL) {
+copy = init_process(tmpsrc->pid, tmpsrc->burst, tmpsrc->priority);
+tmp = copy;
+} else {
+tmp->next = init_process(tmpsrc->pid, tmpsrc->burst, tmpsrc->priority);
+tmp = tmp->next;
+};
+tmpsrc = tmpsrc->next;
+};
+/* Main routine */
+jobsremain = 1;
+slot = NULL;
+while (jobsremain) {
+jobsremain = 0;
+/* Pick next working slot */
+if (slot == NULL) {
+slot = copy;
+jobsremain = 1;
+} else {
+passes = 0;
+do {
+if (slot->next == NULL) {
+passes++;
+slot = copy;
+} else {
+slot = slot->next;
+};
+} while (passes <= 2 && slot->burst == slot->working);
+if (passes <= 2) {
+jobsremain = 1;
+};
+};
+/* Perform a cycle */
+tmp = copy;
+while (tmp != NULL) {
+if (tmp->burst > tmp->working) {
+if (tmp == slot) {
+tmp->working += quantum;
+} else {
+tmp->waiting += quantum;
+};
+};
+tmp = tmp->next;
+};
+};
+
+/* Display statistics and clean up copy */
+tmp = copy;
+while (tmp != NULL) {
+printf("Process: %d\tWorking: %d\tWaiting: %d\tTurnaround: %d\n", 
+tmp->pid, tmp->working, tmp->waiting, tmp->working + tmp->waiting);
+tmpsrc = tmp;
+tmp = tmp->next;
+free(tmpsrc);
+};
+
+printf("END:\tRR scheduling simulation\n\n");
 }
